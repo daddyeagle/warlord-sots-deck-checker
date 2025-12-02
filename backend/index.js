de57580e14b39a9c8ca142cad71f67868a9e1f7d
daddyeagle <olliehappyeagle@hotmail.com>
2025-12-02 04:36:54 +0000

    On branch deploy-docs  Your branch is up to date with 'origin/deploy-docs'.  Changes to be committed: 	modified:   backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 
 // CORS setup: allow requests from frontend (adjust origin as needed)
