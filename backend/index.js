246cfa81a130f6b38b71c933ee87c42f3697ffe7
daddyeagle <olliehappyeagle@hotmail.com>
2026-03-07 23:29:55 +0000

    Changes to be committed: 	modified:   backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -148,7 +148,7 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
   cookie: {
-    secure: process.env.NODE_ENV === 'production' || req.secure || req.headers['x-forwarded-proto'] === 'https',
+    secure: process.env.NODE_ENV === 'production',
     sameSite: 'lax',
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000 // 24 hours
