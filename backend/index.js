30e5e7ae7ee2b870b84c505f916a454a0f3607a4
daddyeagle <olliehappyeagle@hotmail.com>
2026-03-08 00:12:06 +0000

    Changes to be committed: 	modified:   backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -151,7 +151,8 @@ app.use(session({
     secure: process.env.NODE_ENV === 'production',
     sameSite: 'lax',
     httpOnly: true,
-    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    path: '/'
   }
 }));
 
